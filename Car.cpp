b6a591886f2a22f3fe949169227cd55113032d7b
MehdiBouchgua <mehdi.bouchgua@student.hepl.be>
2025-10-08 18:11:24 +0200

    Car./o/cpp/h

--- Car.cpp
+++ Car.cpp
@@ -1,22 +1,84 @@
-/*** Car.h ***/ 
+/*** Car.cpp ***/ 
 
 #include "Car.h"
 #include <string.h>
+#include <iostream>
 
-/******** Constructeur ********/
+using namespace std;
+using namespace carconfig;
 
-Car::Car()
-{
+/******** Constructeur par defaut ********/
+
+Car::Car() {
 	cout << ">>> Car = constructeur par defaut" << endl;
-	name = nullptr;
-	setName("nom");
-	setModel("model");
+	name = "Projet_sans_nom"; //
+	Model m; //model par defaut
+	setModel(m); // assignation de ce modele a la voiture a l'aide du setter
 }
 
-Car::Car(const name& n,const model& m)
-{
+// Constructeur d'initialisation
+
+Car::Car(const string &n,const Model &m) {
 	cout << ">>> Car : constructeur d'initialisation <<<" << endl;
-	name = nullptr;
 	setName(n);
 	setModel(m);
+}
+
+// Constructeur de copie
+
+
+Car::Car(const Car &c) {
+	cout << ">>> Car : Constructeur de copie <<<" << endl;
+	//passage en ref de l'objet c const pour eviter une copie inutile
+	name = c.name;
+	model = c.model; //La copie automatique est safe car string
+
+}
+
+// Destructeur
+
+Car::~Car()
+{
+	cout << ">>> Car : destructeur <<<" << endl;
+	// Rien a désallouer car pas de pointeur ni de mémoire dynamique -> destructeur de "Model" appelé auto
+}
+
+
+// SETTERS
+
+void Car::setName(const string &n)
+{
+	name = n; //affectation directe de la valeur passé en parametre a l'attribut privé "string name"
+}
+
+void Car::setModel(const Model &m)
+{
+	model = m; //On recopie le model passé en paramètre
+}
+
+
+//GETTERS
+
+string Car::getName() const
+{
+	return name; // on retourne une copie du nom et pas de modif de l'objet car const
+}
+
+Model Car::getModel() const
+{
+	return model; // Test2a.cpp getModel().display() donc retour par valeur
+}
+
+
+// Methode Display
+void Car::display() const
+{
+	 //afficher les information objet car complet
+	cout << "=== Projet de voiture ===" << endl;
+	cout << "Nom du projet : " << endl;
+
+	//affichage des caractéristique du model associé
+	cout << "Modèle associé : " << endl;
+	model.display(); //appel dans la classe model
+
 }
--- Car.h
+++ Car.h
@@ -2,33 +2,40 @@
 #ifndef CAR_H
 #define CAR_H
 
-/*#include <iostream>*/
+#include <iostream>
+#include <string>
+#include "Model.h"
+
 using namespace std; // permet d'utiliser cout direment et non stdcout
-/*#include "cstream"*/
+
+namespace carconfig {
+
 
 class Car {
 
 private:
-	string name;
-	Model model;
+	string name; //nom du projet voiture
+	Model model; //Modele de base
 
 public:
 	Car();
-	Car(const string &n, Model &m); // init
-	Car(const Car &c) // copie
+	Car(const string &n,const Model &m); // init
+	Car(const Car &c); // copie
 	~Car(); //Destructeur
 
 	//SETTERS
-	void setName(const char*n);
-	void setModel(const char*c);
+	void setName(const string &n);
+	void setModel(const Model &m);
 
 	//GETTERS
-	string char getName() const;
+	string getName() const;
 	Model getModel() const;
 
 	void display() const;
 
 
 };
+
+} //Fin namespace carconfig
 
 #endif
